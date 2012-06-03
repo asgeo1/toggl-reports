4847d176d321c3999ca7f34c8a23ae7514377434
Adam George <github@adamgeorge.com>
2012-06-03 13:26:07 +1000

    Adding empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TogglReports::Application.config.secret_token = '5cac567c6e818f0cd43d95582c7d8497b0cd33abd4207570cfaf4f2358df12b03cc564fe6d7826eb9a20e6907de458c89df53e84fbbdd19f4d5e7e2686a6f118'
