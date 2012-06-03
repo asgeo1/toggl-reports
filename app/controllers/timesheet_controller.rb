1f21e8f63ce6dcad052fd469b8b72291b29def72
Adam George <github@adamgeorge.com>
2012-06-04 08:34:53 +1000

    Playing around with date + times in ruby
    Getting the start of the logic to divide the timesheet into weeks

--- app/controllers/timesheet_controller.rb
+++ app/controllers/timesheet_controller.rb
@@ -6,10 +6,44 @@ class TimesheetController < ApplicationController
   def imagesoft
     toggl = Toggl.new(ENV['TOGGL_API_KEY'], 'api_token')
 
-    @time_entries = toggl.timeentries({:query => {
-      :start_date => '2012-05-01T00:00:00+10:00',
-      :end_date   => '2012-05-31T23:59:59+10:00'
+    start_date = DateTime.parse('2012-05-01T00:00:00+10:00')
+    end_date   = DateTime.parse('2012-05-31T23:59:59+10:00')
+
+    time_entries = toggl.timeentries({:query => {
+      :start_date => start_date.iso8601,
+      :end_date   => end_date.iso8601,
     }})
+
+    @time_entries = []
+    @weeks = {}
+
+    time_entries['data'].each do |time_entry|
+      if time_entry['project']['client_project_name'] =~
+      /^Dyers|Ausfast|Imagesoft/ then
+        @time_entries << time_entry
+      end
+
+      date       = Date.parse(time_entry['start'])
+      week_start = Date.commercial(date.year, date.cweek, 1)
+      week_end   = Date.commercial(date.year, date.cweek, 7)
+
+      if week_end > end_date then
+        week_end = end_date
+      end
+
+      if week_start < start_date then
+        week_start = start_date
+      end
+
+      if not @weeks.has_key?("#{date.year}_#{date.cweek}") then
+        @weeks["#{date.year}_#{date.cweek}"] = {
+          :start => week_start,
+          :end   => week_end
+        }
+      end
+    end
+
+    # :user_id => 273621
 
     @clients  = toggl.clients()
     @projects = toggl.projects()
